9cfefa9ac89a93623d45c09c79e230b947625d82
luisSantamariaCol <luisestebansantamaria@outlook.com>
2022-11-20 12:09:08 -0500

    Movie filter implemented

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,10 @@ from .models import Movie
 # Create your views here.
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    movies = Movie.objects.all()
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm) # icontains: case insensitive
+    else:
+        movies = Movie.objects.all()
     return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
