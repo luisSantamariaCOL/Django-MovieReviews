25d76ebae5436fb33b3655398ad973799adbfa0d
luisSantamariaCol <luisestebansantamaria@outlook.com>
2022-11-20 19:14:33 -0500

    Error in Listed News corrected

--- news/views.py
+++ news/views.py
@@ -5,4 +5,5 @@ from .models import News
 # Create your views here.
 def news(request):
     newss = News.objects.all()
-    return render(request, 'news.html', {'news': newss})
+    print("number of news: ", len(newss))
+    return render(request, 'news.html', {'newss': newss})
