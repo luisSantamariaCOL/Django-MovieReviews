768a78ef620f06f8fbafba0fe2a297c3c3f92f61
luisSantamariaCol <luisestebansantamaria@outlook.com>
2022-11-20 19:08:39 -0500

    News listed via template

--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import News
+
 
 # Create your views here.
 def news(request):
-    return render(request, 'news.html')
+    newss = News.objects.all()
+    return render(request, 'news.html', {'news': newss})
