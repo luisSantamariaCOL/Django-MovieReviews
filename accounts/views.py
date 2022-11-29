d87c4b4b7bf8832a160ab6c79585ce130be4a853
luisSantamariaCol <luisestebansantamaria@outlook.com>
2022-11-28 21:58:22 -0500

    User created via views

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 # Create your views here.
 def signupaccount(request):
@@ -7,5 +10,12 @@ def signupaccount(request):
         UserCreationFrom is provide by Django to easily create a signup form
         to register new users.
     """
-    return render(request, 'signupaccount.html', {'form': UserCreationForm})
+    if request.method == 'GET':
+        return render(request, 'signupaccount.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+            login(request, user)
+            return redirect('home')
 
