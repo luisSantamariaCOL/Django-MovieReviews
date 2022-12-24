5fa4be2f9ffd570eff8b4616671551ff76c7cd63
luisSantamariaCol <luisestebansantamaria@outlook.com>
2022-12-24 11:40:40 -0500

    comments added to accounts.views

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from .forms import UserCreateForm
-from django.contrib.auth.models import User
-from django.contrib.auth import login
-from django.shortcuts import redirect
-from django.db import IntegrityError    
+from django.shortcuts import render # To render the signupaccount.html template
+from django.contrib.auth.forms import UserCreationForm # This is the form provided by Django to easily create a signup form to register new users
+from .forms import UserCreateForm # This is the form we created in forms.py
+from django.contrib.auth.models import User # To create a new user
+from django.contrib.auth import login # To log the user in after they have successfully created an account
+from django.shortcuts import redirect # To redirect the user to the home page after they have successfully created an account
+from django.db import IntegrityError # To handle the error when a user tries to create an account with a username that already exists    
 
 # Create your views here.
 def signupaccount(request):
@@ -12,10 +12,10 @@ def signupaccount(request):
         UserCreationFrom is provide by Django to easily create a signup form
         to register new users.
     """
-    if request.method == 'GET':
+    if request.method == 'GET': # When the user first visits the page (localhost:8000/accounts/signupaccount), show them a blank form
         return render(request, 'signupaccount.html', {'form': UserCreateForm})
-    else:
-        if request.POST['password1'] == request.POST['password2']:
+    else: # When the user submits the form, create a new user
+        if request.POST['password1'] == request.POST['password2']: 
             try:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 user.save()
