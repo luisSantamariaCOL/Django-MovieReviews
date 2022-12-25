82c318f0c675a4ad23801915312858fa569c4c2d
luisSantamariaCol <luisestebansantamaria@outlook.com>
2022-12-25 11:01:14 -0500

    Review model created

--- movie/models.py
+++ movie/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
-class Movie(models.Model):
+class Movie(models.Model): 
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
 
+class Review(models.Model):
+    text = models.CharField(max_length=250) # stores the review text
+    date = models.DateTimeField(auto_now_add=True) # stores the date the review was created
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # stores the user who created the review. Allos many-to-one relationship
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE) # stores the movie the review is for
+    watchAgain = models.BooleanField(default=False) # stores whether the user would watch the movie again
+
+    def __str__(self):
+        return self.text
 
